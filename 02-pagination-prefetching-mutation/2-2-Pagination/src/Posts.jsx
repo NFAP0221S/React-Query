0341daf9c13012cbe0c1423d8955a3b4e6662988
hansol <vawav11@gmail.com>
2023-01-05 16:09:54 +0900

    페이지네이션 구현

--- 02-pagination-prefetching-mutation/2-2-Pagination/src/Posts.jsx
+++ 02-pagination-prefetching-mutation/2-2-Pagination/src/Posts.jsx
@@ -2,26 +2,36 @@ import { useState } from "react";
 import { PostDetail } from "./PostDetail";
 import { useQuery } from "react-query";
 
-// const maxPostPage = 10;
+const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // 첫번째 파라미터에는 쿼리의 이름, 두번째는 비동기 함수를 입력해야한다.
-  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts, {
-    staleTime: 2000,
-  });
+  const { data, isError, error, isLoading } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+    }
+  );
   // if (!data) return <div />;
   if (isLoading) return <h3>로딩중</h3>;
-  if (isError) return <h3>에러</h3>;
+  if (isError) {
+    return (
+      <>
+        <h3>에러</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
@@ -37,11 +47,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((prev) => prev - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((prev) => prev + 1);
+          }}
+        >
           Next page
         </button>
       </div>
