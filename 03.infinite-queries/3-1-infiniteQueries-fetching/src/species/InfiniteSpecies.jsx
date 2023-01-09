556993eaf4cb3a884eaaa1af292345e182fb4f45
hansol <vawav11@gmail.com>
2023-01-09 15:24:48 +0900

    무한스크롤 실습

--- 03.infinite-queries/3-1-infiniteQueries-fetching/src/species/InfiniteSpecies.jsx
+++ 03.infinite-queries/3-1-infiniteQueries-fetching/src/species/InfiniteSpecies.jsx
@@ -1,6 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
-
 const initialUrl = "https://swapi.dev/api/species/";
 const fetchUrl = async (url) => {
   const response = await fetch(url);
