8bac4936a01e602e6eb5ff1cd1bc40b7b9473865
hansol <vawav11@gmail.com>
2023-01-10 12:36:34 +0900

    오타 삭제

--- 03.infinite-queries/3-2-infiniteQueries-practice/src/species/InfiniteSpecies.jsx
+++ 03.infinite-queries/3-2-infiniteQueries-practice/src/species/InfiniteSpecies.jsx
@@ -1,6 +1,7 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { useInfiniteQuery } from "react-query";
 import { Species } from "./Species";
+
 const initialUrl = "https://swapi.dev/api/species/";
 const fetchUrl = async (url) => {
   const response = await fetch(url);
